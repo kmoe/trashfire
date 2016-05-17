e78fc76ae107600996cea8450bb6a46daa8de38e
Katy Moe <katy@katy.moe>
2016-05-17 22:12:43 +0100

    temp

---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const Hapi = require('hapi');
+const request = require('request');
+const path = require('path');
+const moment = require('moment');
+const sanitizeHtml = require('sanitize-html');
+
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, 'app')
+      }
+    }
+  }
+});
+
+server.connection({
+  port: process.env.PORT || 1337
+});
+
+server.register(require('inert'), (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    log('info', 'Server running at: ' + server.info.uri);
+  });
+});
+
+const io = require('socket.io')(server.listener);
+
+io.on('connection', function(socket){
+  log('client <strong>connected</strong>');
+
+  socket.on('disconnect', function(){
+    log('client <strong>disconnected</strong>');
+  });
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    log('base route');
+    return reply.file('index.html');
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/logs',
+  handler: (request, reply) => {
+    log(request.payload.message);
+    return reply('ok');
+  }
+})
+
+log('Initialising...');
+
+function log() {
+  const message = Array.prototype.slice.call(arguments).join(' ');
+  console.log(message);
+  const sanitisedMessage = sanitizeHtml(message);
+  if (sanitisedMessage !== message) {
+    console.log(sanitisedMessage);
+  }
+  if (io) {
+    io.emit('log', moment().format('YYYY-MM-DD HH:mm:ss') + ': ' + sanitisedMessage);
+  }
+}
